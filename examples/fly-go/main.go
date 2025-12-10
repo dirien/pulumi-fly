e94fa89cd1446cdf2eff33612252f23b58cbdef1
DavidHoenisch <dh1689@pm.me>
2025-12-10 10:31:28 -0800

    FIX: additions to the example to get it working

--- examples/fly-go/main.go
+++ examples/fly-go/main.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/dirien/pulumi-fly/sdk/go/fly"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
+
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		cfg := config.New(ctx, "fly")
+		flyToken := cfg.RequireSecret("fly_api_token")
+
+		// Create explicit provider
+		flyProvider, err := fly.NewProvider(ctx, "fly-provider", &fly.ProviderArgs{
+			FlyApiToken: flyToken,
+		})
+		if err != nil {
+			return err
+		}
 
 		exampleApp, err := fly.NewApp(ctx, "example-app", &fly.AppArgs{
 			Name: pulumi.String("my-dirien-app"),
 			Org:  pulumi.String("personal"),
-		})
+		}, pulumi.Provider(flyProvider))
 		if err != nil {
 			return err
 		}
 
-		_, err = fly.NewIp(ctx, "example-ip", &fly.IpArgs{
+		exampleIpV4, err := fly.NewIp(ctx, "example-ip", &fly.IpArgs{
 			App:  exampleApp.Name,
 			Type: pulumi.String("v4"),
-		})
+		}, pulumi.Provider(flyProvider))
 		if err != nil {
 			return err
 		}
-		_, err = fly.NewIp(ctx, "example-ip-v6", &fly.IpArgs{
+
+		exampleIpV6, err := fly.NewIp(ctx, "example-ip-v6", &fly.IpArgs{
 			App:  exampleApp.Name,
 			Type: pulumi.String("v6"),
-		})
+		}, pulumi.Provider(flyProvider))
+		if err != nil {
+			return err
+		}
 
 		for _, region := range []string{"ams", "cdg", "fra"} {
 
@@ -36,16 +54,20 @@ func main() {
 				Region: pulumi.String(region),
 				Name:   pulumi.Sprintf("my-dirien-app-%s", region),
 				Image:  pulumi.String("dirien/hello-server:latest"),
+				Env: pulumi.StringMap{
+					"FLY_PROCESS_GROUP": pulumi.String("app"),
+					"PRIMARY_REGION":    pulumi.String(region),
+				},
 				Services: fly.MachineServiceArray{
-					fly.MachineServiceArgs{
+					&fly.MachineServiceArgs{
 						Ports: fly.MachineServicePortArray{
-							fly.MachineServicePortArgs{
+							&fly.MachineServicePortArgs{
 								Port: pulumi.Int(80),
 								Handlers: pulumi.StringArray{
 									pulumi.String("http"),
 								},
 							},
-							fly.MachineServicePortArgs{
+							&fly.MachineServicePortArgs{
 								Port: pulumi.Int(443),
 								Handlers: pulumi.StringArray{
 									pulumi.String("tls"),
@@ -59,7 +81,8 @@ func main() {
 				},
 				Cpus:   pulumi.Int(1),
 				Memory: pulumi.Int(256),
-			}, pulumi.DependsOn([]pulumi.Resource{exampleApp}))
+			}, pulumi.DependsOn([]pulumi.Resource{exampleApp, exampleIpV4, exampleIpV6}),
+				pulumi.Provider(flyProvider))
 			if err != nil {
 				return err
 			}
